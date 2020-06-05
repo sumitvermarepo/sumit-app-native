cbcac6a9e836792e6617b5e754573932a5e39804
shubhamvermal <62109911+shubhamvermal@users.noreply.github.com>
2020-06-05 22:08:15 +0530

    change fire base

--- database-config.js
+++ database-config.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyABp3H4nRaK4U09N8V4VyB7RPNGwiyG-Q0",
-    authDomain: "mobile-app-2ce15.firebaseapp.com",
-    databaseURL: "https://mobile-app-2ce15.firebaseio.com",
-    projectId: "mobile-app-2ce15",
-    storageBucket: "mobile-app-2ce15.appspot.com",
-    messagingSenderId: "1053226331924",
-    appId: "1:1053226331924:web:d5064cf5437ff08e2f1c01",
-    measurementId: "G-D83D3YK4RX"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
 };
